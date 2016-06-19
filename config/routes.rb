0499d981a63cfced6633c0e3c9144d8959e60b0d
Justin Chan <justinchanuk@gmail.com>
2016-06-19 03:37:45 +0000

    Update routes so xxx/about and xxx/faq will work without /static

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static#about'
   
-  get 'static/about'
+  get '/about', to: "static#about"
 
-  get 'static/faq'
+  get '/faq', to: "static#faq"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
